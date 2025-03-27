c6fa0b73f6f4f632bf1d50df3a935384f16759a6
eevilleg22 <eddievillegas@tamu.edu>
2025-03-27 18:53:56 -0500

    Update RV32M_MULTIPLIER.vhd

--- RV32M_MULTIPLIER.vhd
+++ RV32M_MULTIPLIER.vhd
@@ -18,8 +18,9 @@ end entity;
 
 architecture RTL of RV32M_MULTIPLIER is
 
-    signal operand_A, operand_B  : signed(32 downto 0);
-    signal product               : signed(63 downto 0);
+    signal operand_A : signed(32 downto 0);
+    signal operand_B : signed(32 downto 0);
+    signal product   : signed(65 downto 0);
 
 begin
 
@@ -28,18 +29,18 @@ begin
         if rising_edge(clock) then
             if enable = '1' then
                 -- Sign Extension Based on Function Type
-                if select_fun = "00" then  -- MUL (Signed × Signed)
+                if select_fun = "00" then  -- MUL (Signed Ã— Signed)
                     operand_A <= signed('0' & input_A);
                     operand_B <= signed('0' & input_B);
-                elsif select_fun = "01" then -- MULH (Signed × Signed, Upper Bits)
+                elsif select_fun = "01" then -- MULH (Signed Ã— Signed, Upper Bits)
                     operand_A <= signed('0' & input_A);
                     operand_B <= signed('0' & input_B);
-                elsif select_fun = "10" then -- MULHSU (Signed × Unsigned)
+                elsif select_fun = "10" then -- MULHSU (Signed Ã— Unsigned)
                     operand_A <= signed('0' & input_A);
-                    operand_B <= signed('0' & std_logic_vector(to_unsigned(to_integer(unsigned(input_B)), 33)));
-                else -- MULHU (Unsigned × Unsigned)
-                    operand_A <= signed('0' & std_logic_vector(to_unsigned(to_integer(unsigned(input_A)), 33)));
-                    operand_B <= signed('0' & std_logic_vector(to_unsigned(to_integer(unsigned(input_B)), 33)));
+                    operand_B <= signed(std_logic_vector(to_unsigned(to_integer(unsigned(input_B)), 33)));
+                else -- MULHU (Unsigned Ã— Unsigned)
+                    operand_A <= signed(std_logic_vector(to_unsigned(to_integer(unsigned(input_A)), 33)));
+                    operand_B <= signed(std_logic_vector(to_unsigned(to_integer(unsigned(input_B)), 33)));
                 end if;
 
                 -- Perform Multiplication
